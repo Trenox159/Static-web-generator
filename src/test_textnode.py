b437578db5c495923f08b12c8ddd21188e08d009
Michal Lenko <misko.lenko@gmail.com>
2026-02-25 19:21:02 +0100

    convert function from text node to html node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -12,6 +12,18 @@ class TestTextNode(unittest.TestCase):
     def test_print(self):
         node = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node.__repr__(), 'TextNode(This is a text node, TextType.BOLD, None)')
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_image(self):
+        node = TextNode('This is nice picture', TextType.IMAGE, url='/public/test.img')
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, 'img')
+        self.assertEqual(html_node.props, {"alt": "This is nice picture", "src": "/public/test.img"})
 
 if __name__ == "__main__":
     unittest.main()
