38ecf15d8b47f28a129afddcc1a8e4d394fb1846
Trenox159 <misko.lenko@gmail.com>
2026-04-16 19:51:36 +0000

    fix

--- src/main.py
+++ src/main.py
@@ -30,7 +30,7 @@ def treeGenerate(src, template, dest, basepath):
 def main():
 
     basepath = sys.argv[1] if len(sys.argv) > 1 else '/'
-
+    print(basepath)
     clean_public()
     os.mkdir('docs')
     copyFiles('static', 'docs')
